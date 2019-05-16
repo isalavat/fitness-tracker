293e121b036330c98ff40172cb2cf445e3d18f84
isalavat <salavat.islamov.96@gmail.com>
2019-05-16 17:48:49 +0200

    First commit. Spring boot project created

---
+++ src/main/java/kg/salavat/finesstracker/saltracker/SaltrackerApplication.java
@@ -0,0 +1,13 @@
+package kg.salavat.finesstracker.saltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltrackerApplication.class, args);
+	}
+
+}
